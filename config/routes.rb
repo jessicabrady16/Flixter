bd8cf2ce07b9f4f808defe3abb88922099448bfb
Jessica Brady <jessicabrady16@gmail.com>
2019-08-29 15:34:35 -0600

    set up sections in course create for instructors, built form section on page, cleaned up views, made submit button work with database, and listed the sections under the course

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: %i[index show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: %i[new create show] do
+      resources :sections, only: %i[new create]
+    end
   end
 end
