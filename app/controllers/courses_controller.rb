fc8f6a34c2f9b051fe5cf630068938170ea31271
Jessica Brady <jessicabrady16@gmail.com>
2019-08-29 13:02:54 -0600

    added more specificity to view more info on course and added admin vs student view on show page of courses and added a model for db migration of sections

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class CoursesController < ApplicationController
   def index
     @courses = Course.all
-end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
 end
--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+#instructor!!!!!!
 class Instructor::CoursesController < ApplicationController
   def new
   @course = Course.new
