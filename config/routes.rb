f0405ba29e0aa248d26ee5ad7bfd9d24bbc3036f
Jessica Brady <jessicabrady16@gmail.com>
2019-08-30 16:50:32 -0600

    in sections, built up lesson model and db, created infrastructure for creating lessons, built a lesson submit form, hooked up db and actions to create button for lessons and displayed lessons in the show section for instructors

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: %i[index show]
+  resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: %i[new create show] do
-      resources :sections, only: %i[new create]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
     end
   end
 end
