9ce997b5afcaf69635fd55e3812ea95ad6793be8
Jessica Brady <jessicabrady16@gmail.com>
2019-09-11 11:59:30 -0600

    added method to require users to be enrolled in the course to view lessons and instructors can view any of their own lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
   def show
   end
 
   private
+  def require_authorized_for_current_lesson
+    unless current_user == current_lesson.section.course.user || current_user.enrolled_courses.include?(current_lesson.section.course)
+      flash[:notice] = "You must enroll in this course to access premium content!"
+      redirect_to current_lesson.section.course
+    end
+  end
 
   helper_method :current_lesson
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-  
+
 end
