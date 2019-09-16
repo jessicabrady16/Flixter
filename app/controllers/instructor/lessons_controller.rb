99e3c5a775be55802037bac2b197b9475de81357
Jessica Brady <jessicabrady16@gmail.com>
2019-09-16 11:42:33 -0600

    used Jquery UI to create a sortable list function with the lessons and updated the JQuery Script with AJAX to send a server get request to update the order of the lessons as the instructor assigns them

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -2,7 +2,8 @@
 
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section
+  before_action :require_authorized_for_current_section, only: %i[new create destroy]
+  before_action :require_authorized_for_current_lesson, only: [:update]
 
   def new
     @lesson = Lesson.new
@@ -19,7 +20,22 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_lesson.update_attributes(lesson_params)
+    render plain: 'updated!'
+  end
+
   private
+  
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
@@ -37,6 +53,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create, :destroy]
+      resources :lessons, only: [:new, :create, :destroy, :edit]
     end
-    resources :courses, only: [:new, :create, :show, :destroy] do
-      resources :sections, only: [:new, :create, :destroy]
+    resources :courses, only: [:new, :create, :show, :destroy, :edit] do
+      resources :sections, only: [:new, :create, :destroy, :edit]
     end
   end
 end
